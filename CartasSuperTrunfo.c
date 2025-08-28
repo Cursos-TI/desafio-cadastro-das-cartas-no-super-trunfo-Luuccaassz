53accdaa88efd5abbd736025696038abdd589be5
Luuccaassz <lucasscoutoperret@gmail.com>
2025-08-28 01:24:38 +0000

    Finalizei o desafio: Nivel Novato Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,111 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main()
+{
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+  // Variaveis Carta 1
 
-    return 0;
+  char estado;
+  char codigocarta[50];
+  char nomecidade[50];
+  int populacao;
+  float area;
+  float pib;
+  int pontosturisticos;
+
+  // Variaveis carta 2
+
+  char estado2;
+  char codigocarta2[50];
+  char nomecidade2[50];
+  int populacao2;
+  float area2;
+  float pib2;
+  int pontosturisticos2;
+
+  // Entrada de dados carta 1
+
+  printf("Digite uma letra que represente o primeiro estado:  \n");
+   scanf(" %c", &estado);
+
+  printf("Digite o código da primeira carta: \n");
+   scanf(" %s", codigocarta);
+
+  printf("Digite o nome da primeira cidade:  \n");
+   // o scanf não reconhece espaços, não seria possivel cadastrar nomes compostos
+   scanf(" %[^\n]", nomecidade);
+
+  printf("Digite a população da primeira carta:  \n");
+   scanf(" %d", &populacao);
+
+  printf("Digite a área em km² da primeira cidade:  \n");
+   scanf(" %f", &area);
+
+  printf("Digite o PIB da primeira cidade:  \n");
+   scanf("%f", &pib);
+
+  printf("Numero de pontos turisticos da primeira cidade:  \n");
+   scanf("%d", &pontosturisticos);
+
+  // entrada de dados carta 2
+
+  printf("Digite uma letra que represente o segundo estado:  \n");
+   scanf(" %c", &estado2);
+
+  printf("Digite o código da segunda carta: \n");
+   scanf(" %s", codigocarta2);
+
+  printf("Digite o nome da segunda cidade:  \n");
+  // o scanf não reconhece espaços, não seria possivel nomes compostos
+   scanf(" %[^\n]", nomecidade2);
+
+  printf("Digite a população da segunda carta:  \n");
+   scanf(" %d", &populacao2);
+
+  printf("Digite a área em km² da segunda cidade:  \n");
+   scanf(" %f", &area2);
+
+  printf("Digite o PIB da segunda cidade:  \n");
+   scanf("%f", &pib2);
+
+  printf("Numero de pontos turisticos da segunda cidade:  \n");
+   scanf("%d", &pontosturisticos2);
+
+  // saida carta 1
+
+  printf("Carta 1  \n");
+
+  printf("Estado: %c \n", estado);
+
+  printf("Código: %s \n", codigocarta);
+
+  printf("Nome da Cidade: %s \n", nomecidade);
+
+  printf("População: %d  \n", populacao);
+
+  printf("Área: %f  \n", area);
+
+  printf("PIB: %f  \n ", pib);
+
+  printf("Número de Pontos Turísticos: %d  \n", pontosturisticos);
+
+  // saida de dados carta 2
+
+  printf("Carta 2  \n");
+
+  printf("Estado: %c \n", estado2);
+
+  printf("Código: %s \n", codigocarta2);
+
+  printf("Nome da Cidade: %s \n", nomecidade2);
+
+  printf("População: %d  \n", populacao2);
+
+  printf("Área: %f  \n", area2);
+
+  printf("PIB: %f  \n ", pib2);
+
+  printf("Número de Pontos Turísticos: %d  \n", pontosturisticos2);
+
+  return 0;
 }
